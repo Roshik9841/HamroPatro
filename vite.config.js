de0cabe5530f13a262a5ca75c2ec7c06643ba2d8
I am Roshik <Roshik9841@gmail.com>
2025-07-09 22:14:45 +0545

    Fixed names

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:"/RecipeApp/",
+  base:"/RecipeApp123/",
   server: {
     open: true, // This will open the browser on npm run dev
   },
